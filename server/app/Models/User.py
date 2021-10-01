5cccba9b8b8a0eeac3959d5f1e6c670cd5dc6030
Abdelrahman Deghedy <abdelrahman.deghedy@tactful.ai>
2021-10-01 08:28:52 +0200

    Some merge fixes

--- server/app/Models/User.py
+++ server/app/Models/User.py
@@ -1,36 +1,11 @@
-<<<<<<< HEAD:server/app/models/auth/authModels.py
-from flask_login import UserMixin
-||||||| 215d456:server/app/auth/models.py
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-=======
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .database import db
 from flask_login import UserMixin
->>>>>>> AO:server/app/Models/User.py
-
-<<<<<<< HEAD:server/app/models/auth/authModels.py
-from app import db
-||||||| 215d456:server/app/auth/models.py
-app = Flask(__name__)
-=======
->>>>>>> AO:server/app/Models/User.py
-
-<<<<<<< HEAD:server/app/models/auth/authModels.py
-class User(UserMixin, db.Model):
-||||||| 215d456:server/app/auth/models.py
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/deghedy/Desktop/UpWntaM3dy/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 
-class User(db.Model):
-=======
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
->>>>>>> AO:server/app/Models/User.py
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False)
     universityId = db.Column(db.Integer, unique=True, nullable=False)
