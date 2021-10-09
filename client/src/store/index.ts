8639b9805972ba7156e69e3822ef304214c8155c
Abdelrahman Deghedy <abdelrahman.deghedy@tactful.ai>
2021-10-09 05:26:15 +0200

    change base url

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -21,7 +21,8 @@ export const store = createStore({
       scrollToAnswer: false,
       currentUser : null,
       token : null,
-      baseUrl : "http://localhost:5000",
+      // baseUrl : "http://localhost:5000",
+      baseUrl : "https://up-wnta-m3dy.herokuapp.com/",
       alternativeImg: 'https://media.istockphoto.com/vectors/male-profile-flat-blue-simple-icon-with-long-shadow-vector-id522855255?k=20&m=522855255&s=612x612&w=0&h=fLLvwEbgOmSzk1_jQ0MgDATEVcVOh_kqEe0rqi7aM5A=',
       mobileResponsive: false,
     };
